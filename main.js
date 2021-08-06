7f66f53b641409225069fb21ea2dbbfef45075c1
DarthJake <metheamazingwebprogrammer@gmail.com>
2021-08-06 13:22:02 -0700

    Trying local import of three.js.

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
 import './style.css'
 
-import * as THREE from 'https://cdn.skypack.dev/three.js';
-import { OrbitControls } from 'https://cdn.skypack.dev/three/examples/jsm/controls/OrbitControls.js';
+import * as THREE from './threejs/build/three.module';
+import { OrbitControls } from './threejs/examples/jsm/controls/OrbitControls';
+
+// import * as THREE from './node_modules/three/build/three.module';
+// import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls';
+
+// import * as THREE from 'https://unpkg.com/three@0.131.3/build/three.js';
+// import { OrbitControls } from 'https://unpkg.com/three@0.131.3/examples/jsm/controls/OrbitControls.js';
+
+// import * as THREE from 'three';
+// import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const scene = new THREE.Scene();
 
